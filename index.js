bf05ed3b1a97b654939cd341239803959c64e05c
YvZySky <ivzysky@gmx.fr>
2017-11-19 15:41:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,56 @@
 //Lancement
 
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-var prefix = ("/");
+const Discord = require("discord.js")
+const bot = new Discord.Client()
 
-bot.login ("Mzc4OTYxNzc1MzI1ODcyMTQw.DOjHMQ.SNs-CoLflPrNVl_BRoCejb96Mjk");
-bot.login(process.env.BOT_TOKEN);
+var prefix = ("/")
 
+
+bot.login ("Mzc4OTYxNzc1MzI1ODcyMTQw.DOjHMQ.SNs-CoLflPrNVl_BRoCejb96Mjk")
 bot.on("ready", () => {
     console.log("Bot Prêt !")
-});
-
-
-
-//Nouveau Arrivants
+})
 bot.on("guildMemberAdd", member => {
     var role = member.guild.roles.find("name", "Les Petites Putes")
     member.addRole(role)
-});
+})
 
 
 
 //Commandes "Les Salopes de Banjolino"
+bot.on("message", function(message) {
+	if(message.author.equals(bot.user)) return;
+	
+	if(!message.content.startsWith(prefix)) return;
+	
+	var args = message.content.substring(prefix.length).split(" ");
+	
+	switch(args[0]) {
+		default:
+			message.channel.sendMessage("Commande Invalide");
+			return;
+
+		case "purge":
+			if(!message.member.roles.some(r=>["DJ"].includes(r.name))) {
+				message.channel.sendMessage("Tu n'as pas le droit, attends d'être DJ")
+				return;
+		
+			message.channel.sendMessage("Le chat va être clean")
+			message.channel.bulkDelete(100)
+			};
+			
+			break;
+	};
+});
+
 
 bot.on("message", message => {
     if (message.content === prefix + "purge"){
-        if(message.member.roles.some(r=>["Les Salopes de Banjolino"].includes(r.name)) ) {
+        if(message.member.roles.some(r=>["Les Salopes de Banjolino"].includes(r.name))) {
         message.channel.sendMessage("Le chat va être clean")
         message.channel.bulkDelete(100)
         }
         else message.channel.sendMessage("Tu n'as pas la permission !")
     }
-});
+})
+
